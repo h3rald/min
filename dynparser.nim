562a95290781747ab9f215c2a5f6f3d52244698b
Peter Munch-Ellingsen <peterme@peterme.net>
2017-10-25 16:58:11 +0200

    Fixed small bug in parser script

--- dynparser.nim
+++ dynparser.nim
@@ -4,9 +4,9 @@ import nre
 var
   currentLine = 1
   oldLine = 0
-  sourceFile = open("parser2.nim")
+  sourceFile = open("core/parser.nim")
 
-for line in lines "parser.ndi":
+for line in lines "core/nimcache/parser.ndi":
   let
     fields = line.split('\t')
     (origName, mangledName, path, row, column) = (fields[0], fields[1], fields[2], fields[3].parseInt, fields[4])
@@ -19,5 +19,5 @@ for line in lines "parser.ndi":
       if definition.startsWith("proc"):
         #echo origName & " -> " & mangledName
         echo definition.replace(re""" *= *\".*\"""", ": string").replace(re"""{\..*\.}""","").replace(re""" *= *$""", "").replace("*(","(") & " {.importc, extern:\"" & mangledName & "\".}"
-      oldLine = currentLine
       currentLine += 1
+      oldLine = currentLine
