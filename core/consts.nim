65da645bfab2e14674d0a4391a54aa56e7f064c7
h3rald <h3rald@h3rald.com>
2018-12-09 17:24:50 +0100

    Moved environment variable retrieval code to env file.

--- core/consts.nim
+++ core/consts.nim
@@ -1,20 +1,6 @@
-import
-  os
-
 const
   pkgName*        = "min"
   pkgVersion*     = "0.19.3"
   pkgAuthor*      = "Fabio Cevasco"
   pkgDescription* = "A tiny concatenative programming language and shell."
 
-
-var HOME*: string
-if defined(windows):
-  HOME = getenv("USERPROFILE")
-if not defined(windows):
-  HOME = getenv("HOME")
-
-let MINRC* = HOME / ".minrc"
-let MINSYMBOLS* = HOME / ".min_symbols"
-let MINHISTORY* = HOME / ".min_history"
-let MINLIBS* = HOME / ".minlibs"
---
+++ core/env.nim
@@ -0,0 +1,12 @@
+import os
+
+var HOME*: string
+if defined(windows):
+  HOME = getenv("USERPROFILE")
+if not defined(windows):
+  HOME = getenv("HOME")
+
+let MINRC* = HOME / ".minrc"
+let MINSYMBOLS* = HOME / ".min_symbols"
+let MINHISTORY* = HOME / ".min_history"
+let MINLIBS* = HOME / ".minlibs"
