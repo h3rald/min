2bb6e6f101adf4e477495fdfa9708082ddda913b
h3rald <h3rald@h3rald.com>
2020-10-03 12:39:14 +0200

    Preparing to release

--- core/consts.nim
+++ core/consts.nim
@@ -1,6 +1,6 @@
 const
   pkgName*        = "min"
-  pkgVersion*     = "0.20.1"
+  pkgVersion*     = "0.21.0"
   pkgAuthor*      = "Fabio Cevasco"
   pkgDescription* = "A tiny concatenative programming language and shell."
 
